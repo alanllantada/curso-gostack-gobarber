b539538e095fe8514d085deaa3a0aaabf38577f5
Alan Guimarães Llantada <alan_llantada@hotmail.com>
2020-02-24 19:40:07 -0300

    Implementação inicial ate a aula Cad. Usuarios

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'gobarber',
+  password: 'Admin123',
+  database: 'gobarber',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
