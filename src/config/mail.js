c852814aeec7f4be908bb2cbcf5e095bca3fc11c
Alan Guimarães Llantada <alan_llantada@hotmail.com>
2020-02-28 17:37:25 -0300

    Impl. até a aula Config templates de email

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '7661d7703f537c',
+    pass: 'f6c1467d17e43b',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
