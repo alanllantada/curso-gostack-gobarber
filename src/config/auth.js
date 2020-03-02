40335677b29a619ee816adcc2628ef046e60a498
Alan Guimarães Llantada <alan_llantada@hotmail.com>
2020-03-02 17:42:33 -0300

    Imple. ate a aula final do modulo do Node.js

--- src/config/auth.js
+++ src/config/auth.js
@@ -1,4 +1,4 @@
 export default {
-  secret: '3481483d519a0ede0a2abe4446b2a0cd',
+  secret: process.env.APP_SECRET,
   expiresIn: '7d',
 };
--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: '2525',
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: '7661d7703f537c',
-    pass: 'f6c1467d17e43b',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe GoBarber <noreply@gobarber.com>',
